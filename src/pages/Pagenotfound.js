3adfdeb54ba0c968c031682e3fcf748fd77b0e78
Niloy Sarkar <138039750+Niloysarkar850@users.noreply.github.com>
2023-07-02 04:46:54 +0530

    Pagenotfound.js

--- src/pages/Pagenotfound.js
+++ src/pages/Pagenotfound.js
@@ -4,7 +4,7 @@ import Layout from './../components/layout/Layout'
 const Pagenotfound = () => {
   return (
     <Layout>
-      <h1>Page not found</h1>
+      // <h1>Page not found</h1>
     </Layout>
   )
 }
